c67e7663f3efe9b1b803483e3e31a51a56d037b6
balves <balves@lopes.com.br>
2023-01-17 06:43:06 -0800

    CORS

--- server.js
+++ server.js
@@ -1,37 +1,25 @@
-// See https://github.com/typicode/json-server#module
-const jsonServer = require('json-server')
-const server = jsonServer.create()
-const router = jsonServer.router('db.json')
-const middlewares = jsonServer.defaults({ noCors: true })
-//Allow CORS in header
-const headers = {
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Headers': 'Content-Type',
-    'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE'
-  };
-  
-    if (event.httpMethod === 'POST' || event.httpMethod === 'DELETE' || event.httpMethod === 'PATCH') {
-        // To enable CORS
-        return {
-            statusCode: 200, 
-            headers,
-            body: 'success'
-        };
-   }
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+const cors = require('cors');
 
-    server.use(middlewares)
-    // Add this before server.use(router)
-    server.use(jsonServer.rewriter({
-        '/*': '/$1',
-    }))
-    server.use(router)
-    server.listen(3000, () => {
-        console.log('JSON Server is running')
+server.use(
+    cors({
+        origin: true,
+        credentials: true,
+        preflightContinue: false,
+        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     })
+);
+server.options('*', cors());
 
-
-    const filePath = path.join("/tmp", "db.json");
-    fs.writeFileSync(filePath, JSON.stringify(data));
+server.use(middlewares);
+server.use(router);
+server.listen(3000, () => {
+    console.log('JSON Server is running');
+});
 
 // Export the Server API
 module.exports = server
+
