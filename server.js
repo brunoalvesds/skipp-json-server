730f520979499a07d28b942b7c9a08252da75fea
balves <balves@lopes.com.br>
2023-01-17 06:18:17 -0800

    CORS

--- server.js
+++ server.js
@@ -3,11 +3,26 @@ const jsonServer = require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults({ noCors: true })
+//Allow CORS in header
+const headers = {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Headers': 'Content-Type',
+    'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE'
+  };
+  
+     if (event.httpMethod === 'POST' || event.httpMethod === 'DELETE' || event.httpMethod === 'PATCH') {
+        // To enable CORS
+        return {
+         statusCode: 200, 
+         headers,
+         body: 'success'
+       };
+   }
 
 server.use(middlewares)
 // Add this before server.use(router)
 server.use(jsonServer.rewriter({
-    '/api/*': '/$1',
+    '/*': '/$1',
     '/blog/:resource/:id/show': '/:resource/:id'
 }))
 server.use(router)
